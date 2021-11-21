2aa5bed0d4111052b0a64dd3cd534899eec2a4e9
Maaashaa <mabolotova@mail.ru>
2021-11-22 02:55:21 +0300

    Initial commit

---
+++ src/Lab2_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Lab2_1 {
+    public static int Factorial(int n){
+        int res = 1;
+            if (n>=0 && n<15){
+                for(int i = 1;i <= n;i++){
+                    res = res*i;
+                }
+            } else {
+                System.out.println("Число должно быть больше или равно 0, но меньше 15.Попробуйте снова.\nВведите число: ");
+            }
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        while(!in.hasNext("close")){
+            if(in.hasNextInt()){
+                System.out.println("Факториал: " + Factorial(in.nextInt()));
+            } else {
+                System.out.println("Ошибка!Нужно целое число.Поробуйте ешё раз.\nВведите число: ");
+                in.next();
+            }
+        }
+        in.close();
+    }
+}
+//не получается убрать вывод факториал:1 после ввода целого числа не из заданного промежутка.подскажите,пожалуйста,как это сделать
